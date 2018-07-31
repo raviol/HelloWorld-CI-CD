bbd9c17d9c207983bc1b3c2e6fc28237cd6d3879
QA Jelastic <andrey.kuznetsov@jelastic.com>
2018-07-31 11:42:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ http.createServer(function (req, res) {
 
     if (path === "/") {
         res.writeHead(200, {"Content-Type": "text/html"});
-        res.write(ejs.render(fs.readFileSync("./index2.ejs", "utf8"), {
+        res.write(ejs.render(fs.readFileSync("./index.ejs", "utf8"), {
             hostname: os.hostname()
         }));
         res.end();
